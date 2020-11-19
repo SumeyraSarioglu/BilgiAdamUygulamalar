cfa8534b2eded62a7a249dbe520f8791aec42a5d
Sümeyra SARIOĞLU <sumeyra.sarioglu@baogrenci.com>
2020-11-19 21:27:36 +0300

    Dort Islem

---
+++ DortIslem/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DortIslem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double sayi1 = double.Parse(txtsayi1.Text);
+            double sayi2 = double.Parse(txtsayi2.Text);
+            MessageBox.Show("sonuc :"+ (sayi1 + sayi2));
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            double sayi1 = double.Parse(txtsayi1.Text);
+            double sayi2 = double.Parse(txtsayi2.Text);
+            MessageBox.Show("sonuc :" + (sayi1 - sayi2));
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            double sayi1 = double.Parse(txtsayi1.Text);
+            double sayi2 = double.Parse(txtsayi2.Text);
+            MessageBox.Show("sonuc :" + (sayi1 / sayi2));
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            double sayi1 = double.Parse(txtsayi1.Text);
+            double sayi2 = double.Parse(txtsayi2.Text);
+            MessageBox.Show("sonuc :" + (sayi1 * sayi2));
+        }
+    }
+}
